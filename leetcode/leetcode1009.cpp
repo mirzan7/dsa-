7605f02eeff41d2706b46cf8b09a52ada08eedd8
Mirzan NP <npmirzanmiraj5@gmail.com>
2023-02-28 22:15:57 +0530

    ..

--- leetcode/leetcode1009.cpp
+++ leetcode/leetcode1009.cpp
@@ -1,24 +1,27 @@
 // C++ code to implement above approach
 #include <bits/stdc++.h>
 using namespace std;
-
 // Function to find the complement
 int findComplement(int num)
 {
-	int ans = 0;
-	for (int i = 0; num >0; i++) {
-		ans += pow(2, i) * (!(num % 2));
-		cout<<"ans "<<ans<<"  i "<<i<<" "<<pow(2,i)<<"  num%2 "<<(num%2)<<"   ! "<<!(num%2)<<endl;
-		num /= 2;
+	int ans=0,i=0;
+	if(num ==0)
+		return 1;
+	else
+	{
+		for(i=0;num>=0;i++)
+		{
+			ans = ans+pow(2,i)*(!(num%2));
+			num = num/2;
+		}
+		cout<<"\n\n\n";
+		return ans;
 	}
-	cout<<"\n\n\n";
-	return ans;
 }
-
 // Driver code
 int main()
 {
-	unsigned int N = 0;
-	cout << findComplement(N);
+	int N = 5;
+	cout <<"compliment   :"<< findComplement(N)<<endl<<endl;
 	return 0;
 }
